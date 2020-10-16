218dc39ec1bd1705eb2e074da83bd4ba63518c0d
Nikolas <34384886+Dudiak-Nikolas@users.noreply.github.com>
2020-10-16 22:37:02 +0300

    Начальный возврат модуля Program

---
+++ Program/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Добро пожаловать в програму в якій можна перевіряти \n"
+                             + " текст на паліндромність і також багато цікавого\n");
+            while (true)
+            {
+                Console.WriteLine("------------------------------------------------------\n "
+                              + "Виберіть що вам потрібно виконати:\n"
+                              + "А - Провірка Цифр на паліндромність;\n"
+                              + "B - Перевести Цифри в Реверс;\n"
+                              + "C - Провірка Текста на паліндромність;\n"
+                              + "Q - Вихід\n");
+
+                var input = Console.ReadKey();
+
+                switch (input.Key)
+                {
+                    
+                   case ConsoleKey.A:
+                            Console.Write("\n Перевірка Числ на паліндромність  \n"
+                                 + "--------------------------------------\n "
+                                 + "Вкажіть Числа : ");
+
+                        try
+                        {
+                            var tf = Solution.IsPalindrome(Convert.ToInt32(Console.ReadLine()));
+                            if (tf == true)
+                                Console.WriteLine(" Ці Числа є паліндромними.\n");
+                            else
+                                Console.WriteLine(" Ці Числа не є паліндромними.\n");
+
+                            Console.Read();
+                        }
+                        catch(FormatException)
+                        {
+                            Console.Write("\nВи ввели не верний формат!\n\n");
+                        }
+
+                            break;
+                        
+
+                    case ConsoleKey.B:
+                        Console.Write("\n Перевести Цифри в Реверс \n"
+                                      + "-------------------------------\n "
+                                      + "Вкажіть Цифри : ");
+                        try
+                        {
+                            var Export = Solution.Reverse(int.Parse(Console.ReadLine()));
+                            checked
+                            {
+                                Console.WriteLine("Цифри в реверсе : " + Export + "\n");
+                            }
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.Write("\nПереповненя\n\n");
+                        }
+                        catch (FormatException)
+                        {
+                            Console.Write("\nВи ввели не верний формат!\n\n");
+                        }
+                        Console.ReadLine();
+                        break;
+
+                    case ConsoleKey.C:
+                        Console.Write("\n Перевірка текста на паліндромність  \n"
+                                + "--------------------------------------\n "
+                                + "Вкажіть текст : ");
+                        var Expo = Solution.IsFilter(Console.ReadLine());
+
+                        if (Expo == true)
+                            Console.WriteLine(" Цей текст є паліндромним.\n");
+                        else
+                            Console.WriteLine(" Цей текст не є паліндромним.\n");
+                       
+                        Console.Read();
+                        break;
+                    case ConsoleKey.Q:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Write("\nВибрана не верная кнопка\n"
+                                        + "Чтоби продолжить нажмите клавишу: 'Enter' ");
+                        Console.ReadLine();
+                        break;
+                   
+                }
+
+            }
+        }
+    }
+}
---
+++ Program/Solution.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+namespace Program
+{
+    public class Solution
+    {
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+            var len = (int)Math.Log10(x);
+
+            while (x != 0)
+            {
+                var low = x % 10;
+                var high = x / (int)Math.Pow(10, len);
+
+                if (low != high) return false;
+                x = x % (int)Math.Pow(10, len) / 10;
+
+                len -= 2;
+            }
+            return true;
+        }
+
+        public static int Reverse(int x)
+        {
+            int rev = 0;
+            while (x != 0)
+            {
+                int list = x % 10;
+        int NewRev = rev * 10 + list;
+                if ((NewRev - list) / 10 != rev)
+                {
+                    break;
+                }
+                rev = NewRev;
+
+                x = x / 10;
+            }
+            return rev;
+        }
+
+        public static bool IsFilter(string y)
+        {
+            if (y != string.Concat(y.Where(x => char.IsLetterOrDigit(x))))
+                return true;
+            else
+            {
+                var newText = string.Concat(y.ToLower()
+                    .Where((a) => char.IsDigit(a) || (a >= 'a' & a <= 'z'))
+                    .Where(x => !char.IsDigit(x))
+                    .ToArray());
+                var export = IsPalindrome_Text(newText);
+                return export;
+            }
+        }
+
+        private static bool IsPalindrome_Text(string x)
+        {
+            if (x.Length <= 1)
+                return true;
+            else
+            {
+                if (x[0] != x[x.Length - 1])
+                    return false;
+                else
+                    return IsPalindrome_Text(x.Substring(1, x.Length - 2));
+            }
+        }
+    }
+}
